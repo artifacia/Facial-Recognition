fbadb103c59173cb02820afda660633ba634b574
snapshopr <vivek@aincubate.com>
2015-08-15 09:02:11 +0530

    one

---
+++ installation.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python-pip
+sudo pip install matplotlib
+sudo apt-get install build-essential python-dev python-setuptools \
+                     python-numpy python-scipy \
+                     libatlas-dev libatlas3gf-base
