568cb533927518a77dd732d1a639e4e91c237eca
snapshopr <vivek@aincubate.com>
2015-08-15 10:35:28 +0530

    updated

--- installation.sh
+++ installation.sh
@@ -3,3 +3,6 @@ sudo pip install matplotlib
 sudo apt-get install build-essential python-dev python-setuptools \
                      python-numpy python-scipy \
                      libatlas-dev libatlas3gf-base
+
+
+sudo pip install scikit-learn
